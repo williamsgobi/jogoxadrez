063e588f08c3a67d3fd5deb8ef80549d42e5a640
William Sgobi <william.sgobi@programaconsumer.com.br>
2021-02-11 11:50:07 -0300

    criado a classe posicao xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,12 +12,6 @@ namespace xadrez_console
 
             try
             {
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-                Tela.imprimirTabuleiro(tab);
-
                 Console.ReadLine();
             } 
             catch(TabuleiroException e)
